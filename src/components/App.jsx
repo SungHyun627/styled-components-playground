fbe9b9c8ba1126b24d10df01110f10abc6b0e4b2
Sunghyun627 <kksscc4@gmail.com>
2022-08-04 14:54:25 +0900

    create animation component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Title from './Title';
 import Buttons from './Buttons';
 import Paragraphs from './Paragraphs';
 import Inputs from './Inputs';
+import Animation from './Animation';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Buttons />
       <Paragraphs />
       <Inputs />
+      <Animation />
     </div>
   );
 }
